f773c340d806ab8c2ad9b0a1fcebdf4f99854c98
Amir Shevat <amir@reshuffle.com>
2020-03-31 13:17:44 -0700

    moved Users data object to stored procedures

--- data-objects/user.js
+++ data-objects/user.js
@@ -3,7 +3,9 @@ class User {
     async getUserBySlackID(slackID, pool){
         
         const client = await pool.connect();
-        let result = await client.query(`SELECT * FROM users WHERE slack_user_id='${slackID}'`);        
+        const query = 'SELECT * FROM users WHERE slack_user_id=$1';
+        const values = [slackID];
+        let result = await client.query(query,values );        
         
         if(result.rows[0]){
             let u = new User()
@@ -32,7 +34,9 @@ class User {
         this.raw = params.raw;
 
         const client = await pool.connect();
-        let res1 = await client.query(`INSERT INTO users(id, slack_user_id, username, name, team_id, raw )VALUES(DEFAULT, '${this.slack_user_id}', '${this.username}', '${this.name}'  , '${this.team_id}' , '${this.raw}' ) RETURNING id`);
+        const query = 'INSERT INTO users(slack_user_id, username, name, team_id, raw )VALUES( $1, $2, $3 , $4 , $5) RETURNING id';
+        const values = [this.slack_user_id, this.username, this.name, this.team_id, this.raw ];
+        let res1 = await client.query(query, values);
         this.id = res1.rows[0].id;
         client.release(); 
         return this;
@@ -40,7 +44,9 @@ class User {
 
     async updateTeamId(team_id, pool){
         const client = await pool.connect();
-        let result = await client.query(`UPDATE users SET team_id='${team_id}' WHERE id='${this.id}'`);
+        const query = 'UPDATE users SET team_id=$1 WHERE id=$2';
+        const values = [team_id, this.id];
+        let result = await client.query(query, values);
         this.team_id = team_id;
         client.release(); 
         return this;
@@ -51,8 +57,9 @@ class User {
         const client = await pool.connect();
         var moment = require('moment');
         let date =  moment().startOf('day').format("YYYY-MM-DD");
-        //console.log(`SELECT * from interview_panelists INNER JOIN users ON interview_panelists.panelist_id = users.id  WHERE interview_panelists.date='${date}' AND users.team_id='${team_id}' AND interview_panelists.notified='0'`);
-        let result = await client.query(`SELECT * from interview_panelists INNER JOIN users ON interview_panelists.panelist_id = users.id INNER JOIN interviews ON interviews.id = interview_panelists.interview_id  WHERE interviews.status='1' AND interview_panelists.date='${date}' AND users.team_id='${team_id}' AND interview_panelists.active='1' AND interview_panelists.notified='0'`);
+        const query = 'SELECT * from interview_panelists INNER JOIN users ON interview_panelists.panelist_id = users.id INNER JOIN interviews ON interviews.id = interview_panelists.interview_id  WHERE interviews.status=$1 AND interview_panelists.date=$2 AND users.team_id=$3 AND interview_panelists.active=$4 AND interview_panelists.notified=$5';
+        const values = [1, date, team_id,1, 0];
+        let result = await client.query(query, values);
         this.team_id = team_id;
         client.release(); 
         return result.rows;        
@@ -61,7 +68,9 @@ class User {
 
     async panalistNotified(panelist_id, interview_id, pool){
         const client = await pool.connect();
-        let result = await client.query(`UPDATE interview_panelists SET notified='1' WHERE panelist_id='${panelist_id}' AND interview_id='${interview_id}'`);
+        const query = 'UPDATE interview_panelists SET notified=$1 WHERE panelist_id=$2 AND interview_id=$3';
+        const values = [1, panelist_id,interview_id ];
+        let result = await client.query(query, values);
         client.release(); 
     }
 
