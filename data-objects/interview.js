5733561cde724fe8cf1c334d85896e4547f7e255
Amir Shevat <amir@reshuffle.com>
2020-04-05 13:26:11 -0700

    fix

--- data-objects/interview.js
+++ data-objects/interview.js
@@ -91,7 +91,8 @@ class Interview {
     async getTemplate(pool) {
         const Template = require('./template');
         const templateDO = new Template(pool);
-        let thisInterviewTemplate = await templateDO.getTemplate(this.role_id, this.role_level_id, this.team_id);
+        let 
+         = await templateDO.getTemplate(this.role_id, this.role_level_id, this.team_id);
         if(!thisInterviewTemplate){
             thisInterviewTemplate = await templateDO.getPublicTemplateByRoleAndLevel(this.role_id, this.role_level_id);
         }
