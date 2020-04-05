4b7ef9b726a923071a77f2ee4ea76c3581f6062b
Amir Shevat <amir@reshuffle.com>
2020-04-05 10:34:14 -0700

    debug

--- index.js
+++ index.js
@@ -561,9 +561,7 @@ express()
           // prompt user to add Competency
           context.org_msg_ts = response.container.message_ts;
           context.org_channel = response.channel.id;
-          console.debug("before");
           let msg = await slackTool.getAddOnsiteInterviewTypeResponse(response.trigger_id, context);
-          console.debug("after");
           //console.log("****view msg"+ JSON.stringify(msg) );
           const fetch = require('node-fetch');
           let debug = await fetch("https://slack.com/api/views.open", {
@@ -646,6 +644,7 @@ express()
         }
         let currentTemplate = null;
         let publicTemplate = null;
+        console.debug("template ID:"+context.template_id);
         if (context.template_id) {
           currentTemplate = await templateDO.getTemplateById(context.template_id);
         } else if (context.role > 0 && context.level > 0 && !context.current_template_id) {
